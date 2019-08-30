20d17045f4851dfc055edab2ffa64646e87cdf55
Sadler <jsadler@usgs.gov>
2019-08-30 11:14:06 -0500

    notes on layers

--- models/layers.py
+++ models/layers.py
@@ -61,6 +61,7 @@ def temporal_conv_layer(x, Kt, c_in, c_out, act_func='relu'):
     '''
     _, T, n, _ = x.get_shape().as_list()
 
+    # This if statement is defining x_input which is used if the activation function is either GLU or ReLU
     if c_in > c_out:
         w_input = tf.get_variable('wt_input', shape=[1, 1, c_in, c_out], dtype=tf.float32)
         tf.add_to_collection(name='weight_decay', value=tf.nn.l2_loss(w_input))
@@ -76,14 +77,20 @@ def temporal_conv_layer(x, Kt, c_in, c_out, act_func='relu'):
     # keep the original input for residual connection.
     x_input = x_input[:, Kt - 1:T, :, :]
 
+    # [jeff] in this if statement it seems like they are getting weights (wt), and biases (bt) and creating a
+    # convolution. The convolution is then adjusted by an activation function
     if act_func == 'GLU':
         # gated liner unit
         wt = tf.get_variable(name='wt', shape=[Kt, 1, c_in, 2 * c_out], dtype=tf.float32)
         tf.add_to_collection(name='weight_decay', value=tf.nn.l2_loss(wt))
         bt = tf.get_variable(name='bt', initializer=tf.zeros([2 * c_out]), dtype=tf.float32)
+        # [jeff] interesting here that they use a "2d" convolution. to me, since it's a temporal dataset, i would have
+        # guessed that it would have been 1d
         x_conv = tf.nn.conv2d(x, wt, strides=[1, 1, 1, 1], padding='VALID') + bt
         return (x_conv[:, :, :, 0:c_out] + x_input) * tf.nn.sigmoid(x_conv[:, :, :, -c_out:])
     else:
+        # [jeff] the only diff between the following line and for 'bt' and the lines above is that c_out is multiplied
+        # by 2
         wt = tf.get_variable(name='wt', shape=[Kt, 1, c_in, c_out], dtype=tf.float32)
         tf.add_to_collection(name='weight_decay', value=tf.nn.l2_loss(wt))
         bt = tf.get_variable(name='bt', initializer=tf.zeros([c_out]), dtype=tf.float32)
@@ -146,8 +153,11 @@ def st_conv_block(x, Ks, Kt, channels, scope, keep_prob, act_func='GLU'):
     :param act_func: str, activation function.
     :return: tensor, [batch_size, time_step, n_route, c_out].
     '''
+    # [jeff] what are the channels?
+    # [jeff] from line 57 (and others)
     c_si, c_t, c_oo = channels
 
+    # [jeff] what is "variable_scope"?
     with tf.variable_scope(f'stn_block_{scope}_in'):
         x_s = temporal_conv_layer(x, Kt, c_si, c_t, act_func=act_func)
         x_t = spatio_conv_layer(x_s, Ks, c_t, c_t)
