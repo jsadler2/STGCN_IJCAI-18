fd24ab4d8bedb326a7d1a6a0ad97bb75234495f6
Sadler <jsadler@usgs.gov>
2019-09-05 13:19:36 -0500

    plotting L, and 'layers' of Lk

--- plot_W.py
+++ plot_W.py
@@ -2,31 +2,35 @@
 from utils.math_graph import weight_matrix
 import matplotlib.pyplot as plt
 import pandas as pd
+from utils.math_graph import scaled_laplacian, cheb_poly_approx
 
-data_file = "data_loader/PeMS-M/PeMS-M/W_228.csv" 
-# plot unweighted
+
+def plot_matrix(matrix, color_bar_label, filename):
+    ax = plt.imshow(matrix, cmap="viridis_r")
+    fig = plt.gcf()
+    fig.colorbar(ax)
+    axes = plt.gca()
+    axes.set_ylabel('Station ID')
+    axes.set_xlabel('Station ID')
+    fig.text(0.97, 0.5, color_bar_label, rotation=270)
+    plt.tight_layout()
+    plt.savefig(filename)
+    plt.clf()
+    plt.cla()
+    plt.close()
+
+
+data_file = "data_loader/PeMS-M/PeMS-M/W_228.csv"
+
 dfw = pd.read_csv(data_file, header=-1)
-ax = plt.imshow(dfw.values, cmap="viridis_r")
-fig = plt.gcf()
-fig.colorbar(ax)
-axes = plt.gca()
-axes.set_ylabel('Station ID')
-axes.set_xlabel('Station ID')
-fig.text(0.97, 0.5, 'distance [?]', rotation=270)
-plt.tight_layout()
-plt.show()
-plt.clf()
-plt.cla()
-plt.close()
-
-
 W = weight_matrix(data_file)
-ax = plt.imshow(W, vmin=0.5, cmap="viridis_r")
-fig = plt.gcf()
-fig.colorbar(ax)
-axes = plt.gca()
-axes.set_ylabel('Station ID')
-axes.set_xlabel('Station ID')
-fig.text(0.97, 0.5, 'weighted distance ', rotation=270)
-plt.tight_layout()
-plt.show()
+L = scaled_laplacian(W)
+Ks = 3
+n = 228
+Lk = cheb_poly_approx(L, Ks, n)
+
+plot_matrix(dfw.values, 'Distance', 'raw distance')
+plot_matrix(L, 'laplacian', 'laplacian')
+plot_matrix(W, 'Weighted Distance', 'weighted_distance')
+for i in range(3):
+    plot_matrix(Lk[:, i*n:(i+1)*n], f'cheb {i}', f'cheb_{i}')
